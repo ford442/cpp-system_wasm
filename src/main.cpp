6304bc7df0b83cfdfa53ee39bb68fef21d2d27ec
Noah Cohn <noahc42@gmail.com>
2023-04-10 06:46:16 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,79 @@
+#include "./main.hpp"
 
+void sys(){
+  
+}
+
+EM_JS(void,js_main,(),{
+
+"use strict";
+
+function normalResStart(){
+setTimeout(function(){
+document.getElementById('shut').innerHTML=2;
+document.getElementById('circle').width=window.innerWidth;
+document.getElementById('circle').height=window.innerHeight;
+document.getElementById('di').click();
+Module.ccall('js_hello');
+},950);
+}
+  
+document.getElementById('pmhig').innerHTML=parseInt(window.innerHeight,10);
+document.getElementById('ihig').innerHTML=parseInt(window.innerHeight,10);
+document.getElementById('scanvas').height=parseInt(window.innerHeight,10);
+document.getElementById('scanvas').width=parseInt(window.innerHeight,10);
+const tem=document.getElementById('tim');
+const ban=document.getElementById('menuBtn');
+const sfr=document.getElementById('slideframe');
+var  $lt=tem.innerHTML;
+
+function grab$lt(){
+$lt=Math.round($lt);
+}
+
+$lt=tem.innerHTML;
+grab$lt();
+const slo=new Slideout({'panel':document.getElementById('panel'),'menu':document.getElementById('menu'),'padding':384,'tolerance':70,'easing':'cubic-bezier(.32,2,.55,.27)'});
+ban.addEventListener('click',function(){slo.toggle();sfr.innerHTML="";
+setTimeout(function(){
+grab$lt();
+slt=$lt/1000;
+slt=Math.round(slt);
+sfr.innerHTML='<input type='+'"te'+'xt" id'+'="time'+'slider"/'+'>';
+const tsl=new rSlider({target:'#timeslider',values:{min:0.25,max:30.00},
+step:[0.25],labels:false,tooltip:true,scale:false,});
+grab$lt();
+slt=($lt/1000);
+slt=slt*100;
+slt=Math.round(slt);
+slt=slt/100;
+tsl.setValues(slt);
+document.getElementById('menu').addEventListener('click',function(){
+$ll=tsl.getValue();
+$ll=$ll*100;
+$ll=Math.round($ll);
+$ll=$ll/100;$ll=($ll*1000);
+tem.innerHTML=$ll;
+});
+setTimeout(function(){
+slt=tem.innerHTML;
+},8);
+},16)
+;});
+document.getElementById('startBtn').addEventListener('click',function(){
+normalResStart();
+});
+setTimeout(function(){
+document.getElementById('circle').width=window.innerWidth;
+document.getElementById('circle').height=window.innerHeight;
+document.getElementById('di').click();
+},550);
+
+});
+
+int main(void){
+  
+js_main();
+return 1;
+
+}
